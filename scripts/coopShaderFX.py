a75004934f573f89440bc535b5e3db4d04a5670e
Santiago Montesdeoca <santymontesdeoca@gmail.com>
2020-02-20 10:55:54 +0200

    Adding referenceCheck() and dereferenceMaterial() functions

--- scripts/coopShaderFX.py
+++ scripts/coopShaderFX.py
@@ -1,17 +1,18 @@
 """
 @license:       MIT
 @repository:    https://github.com/artineering-io/maya-coop
-#                         ____  _               _           _______  __
-#     ___ ___   ___  _ __/ ___|| |__   __ _  __| | ___ _ __|  ___\ \/ /
-#    / __/ _ \ / _ \| '_ \___ \| '_ \ / _` |/ _` |/ _ \ '__| |_   \  / 
-#   | (_| (_) | (_) | |_) |__) | | | | (_| | (_| |  __/ |  |  _|  /  \ 
-#    \___\___/ \___/| .__/____/|_| |_|\__,_|\__,_|\___|_|  |_|   /_/\_\
-#                   |_|                                                
+#                              ____  _               _           _______  __
+#     ___ ___   ___  _ __     / ___|| |__   __ _  __| | ___ _ __|  ___\ \/ /
+#    / __/ _ \ / _ \| '_ \    \___ \| '_ \ / _` |/ _` |/ _ \ '__| |_   \  /
+#   | (_| (_) | (_) | |_) |    ___) | | | | (_| | (_| |  __/ |  |  _|  /  \
+#    \___\___/ \___/| .__/    |____/|_| |_|\__,_|\__,_|\___|_|  |_|   /_/\_\
+#                   |_|
 @summary:       Maya cooperative ShaderFX library
 @run:           import coopShaderFX as csfx (suggested)
 """
 import logging
 import maya.cmds as cmds
+import coopLib as lib
 
 # LOGGING
 logging.basicConfig()  # errors and everything else (2 separate log groups)
@@ -50,12 +51,14 @@ def setNodeValue(material, uniqueNodeName, value, quiet=False):
     nodeId = getId(material, uniqueNodeName, quiet)
     if nodeId:
         selection = cmds.ls(sl=True, l=True)
-        cmds.select(material, r=True)  # triggers are only made if material is selected
+        cmds.select(material, r=True)  # triggers and scripts are only made if material is selected
         if "value" in cmds.shaderfx(sfxnode=material, listProperties=nodeId):
             if isinstance(value, bool):
                 cmds.shaderfx(sfxnode=material, edit_bool=(nodeId, "value", value))
-            if isinstance(value, float):
+            elif isinstance(value, float):
                 cmds.shaderfx(sfxnode=material, edit_float=(nodeId, "value", value))
+            elif isinstance(value, int):
+                cmds.shaderfx(sfxnode=material, edit_int=(nodeId, "value", value))
         else:
             cmds.shaderfx(sfxnode=material, edit_stringlist=(nodeId, "options", value))
         cmds.select(selection, r=True)
@@ -81,3 +84,58 @@ def getNodeValue(material, uniqueNodeName, quiet=False):
         else:
             value = cmds.shaderfx(sfxnode=material, getPropertyValue=(nodeId, "options"))[-1]
     return value
+
+
+def referenceCheck(mat, promptViewed=False):
+    """
+    Checks if the material is referenced and processes it accordingly
+    Args:
+        mat (str): material to check
+    """
+    # check if material is referenced
+    if cmds.referenceQuery(mat, isNodeReferenced=True) and not promptViewed:
+        message = "Referenced ShaderFX materials may not save correctly\n" \
+                  "Do you wish to convert them to local materials instead?"
+        button = ['Yes', 'Yes, convert ALL', 'No']
+        reply = cmds.confirmDialog(t="Referenced materials", m=message, b=button, icn="warning", ma="center")
+        promptViewed = True
+        if "Yes" in reply:
+            if "Yes" == reply:
+                if lib.getAssignedMeshes(mat):
+                    mat = dereferenceMaterial(mat)
+            else:
+                materials = cmds.ls(type="ShaderfxShader")
+                for m in materials:
+                    if m != mat:
+                        if lib.getAssignedMeshes(m):
+                            dereferenceMaterial(m)
+                if lib.getAssignedMeshes(mat):
+                    mat = dereferenceMaterial(mat)
+    return mat
+
+
+def dereferenceMaterial(mat):
+    """
+    Creates a local material with the same name if mat is referenced
+    Args:
+        mat (str): Material name to check and dereference
+    Returns:
+        localMaterial (str): Local material name
+    """
+    logger.debug("Dereferencing {0}".format(mat))
+    if cmds.referenceQuery(mat, isNodeReferenced=True):
+        newMaterial = mat.split(':')[-1]
+        objects = lib.getAssignedMeshes(mat, shapes=True, l=True)
+        # Method 1: By duplicating the material and assigning it to objects
+        newMaterial = cmds.duplicate(mat, n=newMaterial)[0]
+        lib.setMaterial(newMaterial, objects)
+        # Method 2:  By saving and writing attributes in new material (slower and relies on MNPRX)
+        """
+        matAttrs = dict()
+        getMaterialAttrs(mat, matAttrs)
+        newMaterial = createMaterial(objects, newMaterial, matAttrs["graph"])
+        setMaterialAttrs(newMaterial, matAttrs)
+        """
+        return newMaterial
+    else:
+        return mat
